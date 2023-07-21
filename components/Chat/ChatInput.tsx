3be1e9c85aeb9dbc010d604557ec2db26ff41a66
Yash Bharat Rathod <76907769+YASHBITU1@users.noreply.github.com>
2023-07-21 20:44:54 +0530

    Update ChatInput.tsx

--- components/Chat/ChatInput.tsx
+++ components/Chat/ChatInput.tsx
@@ -325,7 +325,7 @@ export const ChatInput = ({
               }`,
             }}
             placeholder={
-              t('Hey there Iam TARA... type "/" to select a prompt...') || ''
+              t('Type a message or type "/" to select a prompt...') || ''
             }
             value={content}
             rows={1}
@@ -381,16 +381,16 @@ export const ChatInput = ({
       </div>
       <div className="px-3 pt-2 pb-3 text-center text-[12px] text-black/50 dark:text-white/50 md:px-4 md:pt-3 md:pb-6">
         <a
-          href="https://www.instagram.com/taragpt.in"
+          href="https://github.com/mckaywrigley/chatbot-ui"
           target="_blank"
           rel="noreferrer"
           className="underline"
         >
-          Taragpt.in
+          ChatBot UI
         </a>
         .{' '}
         {t(
-          "TARA is an AI chatbot assistant designed to provide valuable support and engagement to users through natural language processing and artificial intelligence technologies.",
+          "Chatbot UI is an advanced chatbot kit for OpenAI's chat models aiming to mimic ChatGPT's interface and functionality.",
         )}
       </div>
     </div>
