6a5b7660c5207fa95d6d83f5c24df3d3f7be3762
Yash Bharat Rathod <76907769+YASHBITU1@users.noreply.github.com>
2023-07-21 19:59:55 +0530

    Add files via upload

--- components/Chat/ChatLoader.tsx
+++ components/Chat/ChatLoader.tsx
@@ -10,7 +10,7 @@ export const ChatLoader: FC<Props> = () => {
     >
       <div className="m-auto flex gap-4 p-4 text-base md:max-w-2xl md:gap-6 md:py-6 lg:max-w-2xl lg:px-0 xl:max-w-3xl">
         <div className="min-w-[40px] items-end">
-          <img size={30} />
+          <img src="/star.png" width="30px" height="30px" />
         </div>
         <span className="animate-pulse cursor-default mt-1">▍</span>
       </div>
